c0bb288719c38eaf69b6b7e9f5602f39e5c122f2
ykun <transam3y@gmail.com>
2015-10-31 11:38:31 +0900

    add new file

---
+++ Editor/TwitterWindow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Reflection;
+
+//webviewはちょっと色々アレかもしれない
+public class TwitterWindow : EditorWindow {
+    private const BindingFlags _flags_ = BindingFlags.Public | BindingFlags.Static;
+
+    [MenuItem("Window/Twitter")]
+    private static void Open() {
+        var type = Types.GetType("UnityEditor.Web.WebViewEditorWindow", "UnityEditor.dll");
+        var methodInfo = type.GetMethod("Create", _flags_);
+        methodInfo = methodInfo.MakeGenericMethod(typeof(TwitterWindow));
+        methodInfo.Invoke(null, new object[]{"Twitter", "https://mobile.twitter.com/", 300, 520, 520, 600});
+    }
+}
